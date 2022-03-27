93cc27fcdeaeab0809add23df266e846d0ea5af6
JoseParSeq <102470671+JoseParSeq@users.noreply.github.com>
2022-03-27 15:22:45 +0200

    ejercicio2_1

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,24 +1,5 @@
 diccionario={}
 
-class empleado (list):
-    def __init__(self, nif, nombre, DOB, sueldom, tipo, var1, var2):
-        self.nif=nif
-        self.nombre=nombre
-        self.DOB=DOB
-        self.sueldom=sueldom
-        self.tipo=tipo
-        self.var1=var1
-        self.var2=var2
-
-        self.data=nombre + ", " + DOB + ", " + sueldom + ", " + tipo + ", " + var1 + ", " + var2
-        diccionario[self.nif]=self.data
-
-    def datos(self):
-        return self.nombre
-
-    def add(self, NIF, nombre, DOB, sueldom, tipo):
-        return
-
 def pedir_num():
     print("Menu de opciones")
     print("(1) Añadir empleado")
